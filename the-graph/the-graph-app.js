e451dfc1731e111f12a5690d4b86fbeb84f82782
Jeff Greenberg <jeff@appliedworlds.com>
2019-05-22 18:52:54 -0700

    feat: Attempt to allow hammerJS pan events to be triggered by right-click/drag in addition to the standard left-click/drag.

--- the-graph/the-graph-app.js
+++ the-graph/the-graph-app.js
@@ -1,5 +1,53 @@
 var hammerhacks = require('./hammer.js');
 
+//extend to allow right click
+//input mouse map is not a public property of Hammer, so copy it here
+var MOUSE_INPUT_MAP = {
+  mousedown: Hammer.INPUT_START,
+  mousemove: Hammer.INPUT_MOVE,
+  mouseup: Hammer.INPUT_END
+};
+//override
+Hammer.inherit(Hammer.MouseInput, Hammer.Input, {
+  /**
+   * handle mouse events
+   * @param {Object} ev
+   */
+  handler: function MEhandler(ev) {
+      var eventType = MOUSE_INPUT_MAP[ev.type];
+
+      //modified to handle all buttons
+      //left=0, middle=1, right=2
+      if (eventType & Hammer.INPUT_START) {
+          //firefox sends button 0 for mousemove, so store it here
+          if(this.pressed === false) this.button = ev.button;
+          this.pressed = true;
+      }
+
+      if (eventType & Hammer.INPUT_MOVE && ev.which === 0) {
+          eventType = Hammer.INPUT_END;
+      }
+      // mouse must be down, and mouse events are allowed (see the TouchMouse input)
+      if (!this.pressed || !this.allow) {
+          return;
+      }
+
+      if (eventType & Hammer.INPUT_END) {
+          this.pressed = false;
+          this.button = false;
+      }
+
+      this.callback(this.manager, eventType, {
+                      button: this.button,
+          pointers: [ev],
+          changedPointers: [ev],
+          pointerType: 'mouse',
+          srcEvent: ev
+      });
+  }
+});
+
+
 // Trivial polyfill for Polymer/webcomponents/shadowDOM element unwrapping
 var unwrap = (window.unwrap) ? window.unwrap : function(e) { return e; };
 
